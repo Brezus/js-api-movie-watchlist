5a4f6f86519c3b2b88cb8d2e917d3660cf8c5945
Casey Ocampo <caseysoflo@gmail.com>
2022-02-25 15:56:25 -0500

    Add responsive header

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 let imdbId = 'tt3896198' // IMBd ID
 const searchInput = document.getElementById('searchInput')
 const searchBtn = document.getElementById('searchBtn')
+const moviesList = document.getElementById('moviesList')
+const movieListDefaultDisplay = document.getElementsByClassName(
+    'movie-list-default-display'
+)
 
 searchBtn.addEventListener('click', searchMovies)
 
 function searchMovies() {
-    // console.log(searchInput.value)
-    fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=e668e570`) // IMDb Movie title to search for
+    // IMDb Movie title to search for
+    fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=e668e570`)
         .then(res => res.json())
         .then(data => {
-            // console.log(data.Search)
+            console.log(data.Search)
             const movies = data.Search
-            movies.forEach(movie => console.log(movie))
+
+            for (let element of movieListDefaultDisplay) {
+                element.style.display = 'none'
+            }
+
+            movies.forEach(movie => {
+                console.log(movie)
+                moviesList.innerHTML += `
+                <h2>${movie.Title}</h2>
+            `
+            })
         })
 }
-
-// fetch(`http://www.omdbapi.com/?i=${imdbId}&apikey=e668e570`) // IMDb ID
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//     })
