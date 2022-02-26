c3d8a100aea714cb292d6336974e70552639cf9c
Casey Ocampo <caseysoflo@gmail.com>
2022-02-26 06:37:23 -0500

    Remove input bg image

--- index.js
+++ index.js
@@ -5,26 +5,22 @@ const movieListDefaultDisplay = document.getElementsByClassName(
     'movie-list-default-display'
 )
 
-// searchBtn.addEventListener('click', function () {
-//     window.alert(`${searchInput.value}`)
-// })
-
 searchBtn.addEventListener('click', searchMovies)
 
-async function searchMovies() {
-    const res = await fetch(
-        `http://www.omdbapi.com/?s=${searchInput.value}&apikey=e668e570`
-    )
-    const data = await res.json()
-    const movies = data.Search
+function searchMovies() {
+    fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=e668e570`)
+        .then(res => res.json())
+        .then(data => {
+            const movies = data.Search
 
-    for (let element of movieListDefaultDisplay) {
-        element.style.display = 'none'
-    }
+            for (let element of movieListDefaultDisplay) {
+                element.style.display = 'none'
+            }
 
-    movies.forEach(movie => {
-        moviesList.innerHTML += `
-        <h2>${movie.Title}</h2>
-    `
-    })
+            movies.forEach(movie => {
+                moviesList.innerHTML += `
+                <h2>${movie.Title}</h2>
+            `
+            })
+        })
 }
