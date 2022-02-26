1fd8d8aa6f35e045599f1b4ff1efbfca681d0a58
Casey Ocampo <caseysoflo@gmail.com>
2022-02-26 12:50:49 -0500

    Add second fetch to get movie list data

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A solo project from Scrimba (all code is from scratch). The only resource provided was a Figma design file and a link to use the OMDb API.
 
-This project uses the [OMBd API](http://www.omdbapi.com/).
+This project uses the [OMBd API](https://www.omdbapi.com/).
--- old-index.js
+++ old-index.js
@@ -27,29 +27,16 @@ async function searchMovies() {
         element.style.display = 'none'
     }
 
-    const movies = data.Search // cant use this
+    const movies = data.Search
+    // console.log(data.Search)
 
-    movies.forEach(async movie => {
-        let response = await fetch(
-            `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=e668e570`
-        )
-        let moviesListData = await response.json()
-        console.log(moviesListData)
+    movies.forEach(movie => {
         moviesList.innerHTML += `
-                <div>
-                    <div>
-                        <img src=${moviesListData.Poster} />
-                    </div>
-                    <div>
-                        <h2>${moviesListData.Title}</h2>
-                        <img src="images/star-icon.svg" />
-                        <p>${moviesListData.imdbRating}</p>
-                        <p>${moviesListData.Runtime}</p>
-                        <p>${moviesListData.Genre}</p>
-                        <p>Watchlist</p>
-                        <p>${moviesListData.Plot}</p>
-                    </div>    
-                </div>
-            `
+        <div>
+            <img src=${movie.Poster} />
+            <h2>${movie.Title}</h2>
+
+        </div>
+    `
     })
 }
