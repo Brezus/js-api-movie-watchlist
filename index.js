1ab8d76992c924909e53733295d3c552494a4621
Casey Ocampo <caseysoflo@gmail.com>
2022-02-26 06:14:39 -0500

    Remove test elements

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-// const searchInput = document.getElementById('searchInput')
-// const searchBtn = document.getElementById('searchBtn')
+const searchInput = document.getElementById('searchInput')
+const searchBtn = document.getElementById('searchBtn')
 const moviesList = document.getElementById('moviesList')
 const movieListDefaultDisplay = document.getElementsByClassName(
     'movie-list-default-display'
 )
 
-const searchBtn = document.getElementById('searchBtn')
-const searchInput = document.getElementById('searchInput')
+searchBtn.addEventListener('click', searchMovies)
 
-searchBtn.addEventListener('click', function () {
-    window.alert(`${searchInput.value}`)
-})
+function searchMovies() {
+    // IMDb Movie title to search for
+    fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=e668e570`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.Search)
+            const movies = data.Search
 
-// searchBtn.addEventListener('click', searchMovies)
+            for (let element of movieListDefaultDisplay) {
+                element.style.display = 'none'
+            }
 
-// function searchMovies() {
-//     // IMDb Movie title to search for
-//     fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=e668e570`)
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log(data.Search)
-//             const movies = data.Search
-
-//             for (let element of movieListDefaultDisplay) {
-//                 element.style.display = 'none'
-//             }
-
-//             movies.forEach(movie => {
-//                 console.log(movie)
-//                 moviesList.innerHTML += `
-//                 <h2>${movie.Title}</h2>
-//             `
-//             })
-//         })
-// }
+            movies.forEach(movie => {
+                console.log(movie)
+                moviesList.innerHTML += `
+                <h2>${movie.Title}</h2>
+            `
+            })
+        })
+}
