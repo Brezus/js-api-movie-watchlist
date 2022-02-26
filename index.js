af885edbdc325a8162920b5aaa2418883a49691a
Casey Ocampo <caseysoflo@gmail.com>
2022-02-26 06:31:38 -0500

    Add async await

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ const movieListDefaultDisplay = document.getElementsByClassName(
 
 searchBtn.addEventListener('click', searchMovies)
 
-function searchMovies() {
-    fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=e668e570`)
-        .then(res => res.json())
-        .then(data => {
-            const movies = data.Search
+async function searchMovies() {
+    const res = await fetch(
+        `http://www.omdbapi.com/?s=${searchInput.value}&apikey=e668e570`
+    )
+    const data = await res.json()
+    const movies = data.Search
 
-            for (let element of movieListDefaultDisplay) {
-                element.style.display = 'none'
-            }
+    for (let element of movieListDefaultDisplay) {
+        element.style.display = 'none'
+    }
 
-            movies.forEach(movie => {
-                moviesList.innerHTML += `
-                <h2>${movie.Title}</h2>
-            `
-            })
-        })
+    movies.forEach(movie => {
+        moviesList.innerHTML += `
+        <h2>${movie.Title}</h2>
+    `
+    })
 }
